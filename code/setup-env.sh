489bb47af2f896f963eca5731e4953dc68e3d9aa
Pavel Ivashkov <pavels.code@gmail.com>
2017-08-04 17:46:18 +0300

    + python venv

---
+++ code/setup-env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if hash virtualenv 2>/dev/null
+then
+  VENV="virtualenv"
+else
+  VENV="python -m venv"
+fi
+
+$VENV -p python3 .virtualenv/icfpc
+
+. activate
+
+pip install -U setuptools
+pip install -U pip
+pip install -r requirements.txt
