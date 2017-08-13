619d20ccb3df5a380312da4ca30bbb84510b33e9
Pavel Ivashkov <pavels.code@gmail.com>
2017-08-13 17:16:11 +0300

    :cloud: compose hosting

---
+++ hosting/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+PUBLIC_URL="$1"
+
+if [ -z "$PUBLIC_URL" ]; then
+    echo 'usage: ./install.sh public_url'
+    exit 1
+fi
+
+
+sed -e "s/TEMPLATE_HOST/$PUBLIC_URL/g" docker-compose.template.yml > docker-compose.yml
+
+
+# pip install -r requirements.txt
+# docker-compose run -d --build
